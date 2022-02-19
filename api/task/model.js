e4877ae2c51e1e6add70b830d2b71aea97ba3856
Khaleel Musleh <khaleelmusleh@Khaleels-MacBook-Pro.local>
2022-02-18 17:38:07 -0800

    almost done

--- api/task/model.js
+++ api/task/model.js
@@ -1,32 +1,54 @@
 const db = require("../../data/dbConfig");
 
 
-function getTask() {
-    return db("projects as pj")
-    .select('ts.*', 'pj.project_name', 'pj.project_description')
-    .leftJoin('tasks as ts', 'pj.project_id', 'ts.project_id')
+async function getTask() {
+  
+    const tasks = await db('tasks as ts')
+ .leftJoin('projects as pj', 'pj.project_id', '=' ,'ts.project_id')
+  .select('ts.*', 'pj.project_name', 'pj.project_description')
+
+//  .where('pj.project_id', 'ts.project_id')
+
+//  db('schemes as sc')
+//  .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+//  .where('sc.scheme_id', scheme_id)
+//  .select('ts.*', 'pj.project_name', 'pj.project_')
+//  .orderBy('st.step_number', 'asc')
+
+    let finalTasks = [];
+
+    tasks.forEach(task => {
+
+      const taskResolved = {
+        task_id: task.task_id,
+        task_description: task.task_description,
+        task_notes: task.task_notes,
+        task_completed: Boolean(task.task_completed),
+        project_id: task.project_id,
+        project_name: task.project_name,
+        project_description: task.project_description
+      }
+
+      finalTasks.push(taskResolved)
+
+    //   - [ ] `task_id` - primary key
+    //   - [ ] `task_description` - required
+    //   - [ ] `task_notes` - optional
+    //   - [ ] `task_completed` - the database defaults it to `false` (integer 0) if not provided
+    //   - [ ] `project_id` - required and points to an actual `project_id` in the `projects` table
+      
+    })
+
+    return finalTasks;
 }
 
-function findById(id) {
-  return db("tasks")
-    .where({ id })
-    .first();
+
+ async function createTask(task) {
+const insertTask = await db('tasks').insert(task)
+return db('tasks').where('task_id', insertTask).first() 
 }
 
-// async function add(hub) {
-//   const [id] = await db('hubs').insert(hub);
-
-//   return findById(id);
-// }
-
-
-function createTask(task) {
-return db("tasks").insert(task);
-
-  // return getAnimals().where({ project_id }).first();
-}
 
 module.exports = { 
   getTask,
-  findById,
   createTask };
--- api/task/router.js
+++ api/task/router.js
@@ -6,15 +6,28 @@ const Tasks = require('./model')
 
 router.get('/', async (req, res) => {
     const allTasks = await Tasks.getTask();
+    // req.body.project_completed === 1 ? true : false
 
     res.json(allTasks)
 })
 
 router.post('/', async (req, res) => {
-    const insertProject = await Tasks.createTask(req.body)
+    const insertTask = await Tasks.createTask(req.body)
 
-    res.json(insertProject)
+    res.status(201).json({
+        status: 201, 
+        task_id: insertTask.task_id,
+        task_description: insertTask.task_description,
+        task_notes: insertTask.task_notes,
+        task_completed: Boolean(insertTask.task_completed),
+        project_id: insertTask.project_id  
+     })
 })
 
+router.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+      message: err.message
+    })
+  })
 
-module.exports = router;
+  module.exports = router;
